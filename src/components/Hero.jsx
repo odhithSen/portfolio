ae8cd18cf67c7beeee1696fb4d248a6cd1caf314
OdhithSen <odhith.sen@gmail.com>
2024-07-03 13:11:02 +0530

    Update profile section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,41 @@
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
+import { me } from "../assets";
 
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className="paddingX absolute inset-0 top-[80px] md:top-[150px] max-w-7xl 
+        mx-auto flex flex-col md:flex-row items-center md:items-start gap-5"
       >
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
-        </div>
+        <img
+          src={me}
+          alt="Odhith"
+          className="w-[220px] h-[220px] md:w-[350px] 
+        md:h-[350px] rounded-full border-[5px] border-[#915eff] hover:scale-105 
+        transition-all ease-in-out duration-500 ml-5"
+        />
+        <div className="flex flex-row gap-5 ">
+          <div className="flex flex-col justify-center items-center mt-5">
+            <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+            <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          </div>
 
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Odhith</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            BSc. (Hons) Computer Science (Reading)
-            <br className="sm:block hidden" />
-            Entrepreneur | Robotics and Tech Enthusiast
-          </p>
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>
+              Hi, I'm <span className="text-[#915EFF]">Odhith</span>
+            </h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              Computer Science Undergraduate
+              <br className="sm:block hidden" />
+              Entrepreneur | Robotics and Tech Enthusiast
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-20 bottom-10 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
